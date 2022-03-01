41698227bef4b4d90e43fb18bacf693d290ece04
yasiloghmani <yasaman.loghmani@elasticpath.com>
2022-03-01 17:04:52 -0500

    test

---
+++ pages/products/[...slug].tsx
@@ -0,0 +1,37 @@
+import {getProductById, getAllPCMProducts} from '../../services/services'
+
+export default function Product({products}) {
+    return (
+        <div>
+            <h1>Product</h1>
+            {console.log(products)}
+            <h1>{products.attributes.name}</h1>
+        </div>
+    )
+}
+
+export async function getStaticProps( { params } ) {
+    const products = await getProductById(params.slug)
+    return {
+        props: {
+            products: products,
+        },
+    }
+}
+
+
+export async function getStaticPaths() {
+  const products = await getAllPCMProducts()
+  return {
+    paths: products.map(
+      product => {
+        return {
+          params: {
+            slug:[ product.id]
+          }
+        }
+      }
+    ),
+    fallback: false
+  }
+}
