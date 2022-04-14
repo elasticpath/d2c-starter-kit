0ae31d23c3274b6a3577c26160b9ed7c84fbc9e3
Robert Field <robertfield@quadrimular.com>
2022-04-14 12:45:17 +0100

    refactor: temp  casting and ts-ignore
    will update moltin js-sdk once I have permission

--- services/hierarchy.ts
+++ services/hierarchy.ts
@@ -9,9 +9,9 @@ export async function getHierarchies(): Promise<EPCC.Hierarchy[]> {
 }
 
 export async function getNodes(hierarchyId: string): Promise<EPCC.Node[]> {
-  const result = await EPCCAPI.Catalog.Hierarchies.GetHierarchyChildren({
+  const result = (await EPCCAPI.Catalog.Hierarchies.GetHierarchyChildren({
     hierarchyId,
-  });
+  })) as unknown as EPCC.ResourceList<EPCC.Node>; // TODO update the js-sdk to use Node instead of Hierarchy
   const nodes = result.data;
   return nodes;
 }
@@ -19,6 +19,7 @@ export async function getNodes(hierarchyId: string): Promise<EPCC.Node[]> {
 export async function getNodesProducts(
   nodeId: string
 ): Promise<EPCC.ResourcePage<PcmProduct>> {
+  // @ts-ignore TODO remove this and handle issue
   const productsList = await EPCCAPI.Catalog.Nodes.GetNodeProducts({ nodeId });
   return productsList;
 }
