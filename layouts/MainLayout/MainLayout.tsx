0d3495b3b0e04b8b8e793cc6941d4abed7e5a77d
Robert Field <robertfield@quadrimular.com>
2022-04-14 11:47:39 +0100

    refactor: typed props and return

--- layouts/MainLayout/MainLayout.tsx
+++ layouts/MainLayout/MainLayout.tsx
@@ -1,7 +1,13 @@
 import Footer from "../../components/Footer/Footer";
 import MainMenu from "../../components/MainMenu/MainMenu";
 
-const MainLayout = ({ children }) => {
+import type { ReactNode } from "react";
+
+interface IMainLayout {
+  children: ReactNode;
+}
+
+const MainLayout = ({ children }: IMainLayout): JSX.Element => {
   return (
     <div>
       <MainMenu />
