17b30f33e24e4b7d4330ff398e87d704896c3e60
Robert Field <robertfield@quadrimular.com>
2022-04-15 10:49:12 +0100

    refactor: typed and cleaned up products service

--- services/products.ts
+++ services/products.ts
@@ -3,16 +3,14 @@ import { EPCCAPI } from "./helper";
 
 export async function getProductById(
   productId: string
-): Promise<EPCC.ProductResponse> {
-  const result = await EPCCAPI.Catalog.Products.With("main_image").Get({
+): Promise<EPCC.Resource<EPCC.ProductResponse>> {
+  return await EPCCAPI.Catalog.Products.With("main_image").Get({
     productId,
   });
-  const product = result;
-  return product;
 }
 
-export async function getAllPCMProducts(): Promise<EPCC.PcmProduct[]> {
-  const result = await EPCCAPI.Catalog.Products.All();
-  const product = result.data;
-  return product;
+export async function getAllProducts(): Promise<
+  EPCC.ResourceList<EPCC.ProductResponse>
+> {
+  return await EPCCAPI.Catalog.Products.All();
 }
