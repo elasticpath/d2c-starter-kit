8d16b06a3bba21bdf62140f0602bfb1e8e38756b
Robert Field <robertfield@quadrimular.com>
2022-04-25 11:01:11 +0100

    feat(product): utility for filtering product files

---
+++ __tests__/product-util.test.ts
@@ -0,0 +1,164 @@
+import type { File, ProductResponse, Resource } from "@moltin/sdk";
+import {
+  getProductOtherImageUrls,
+  processImageFiles,
+} from "../lib/product-util";
+
+describe("product util", () => {
+  it("processImageFiles should return only supported images without the main image", () => {
+    const files: Partial<File>[] = [
+      {
+        type: "file",
+        id: "123",
+        mime_type: "image/jpeg",
+      },
+      {
+        type: "file",
+        id: "456",
+        mime_type: "image/gif",
+      },
+      {
+        type: "file",
+        id: "789",
+        mime_type: "image/jpeg",
+      },
+      {
+        type: "file",
+        id: "101112",
+        mime_type: "image/png",
+      },
+      {
+        type: "file",
+        id: "131415",
+        mime_type: "image/svg+xml",
+      },
+      {
+        type: "file",
+        id: "161718",
+        mime_type: "image/webp",
+      },
+      {
+        type: "file",
+        id: "192021",
+        mime_type: "video/mp4",
+      },
+      {
+        type: "file",
+        id: "222324",
+        mime_type: "application/pdf",
+      },
+      {
+        type: "file",
+        id: "252627",
+        mime_type: "application/vnd.ms-excel",
+      },
+      {
+        type: "file",
+        id: "282930",
+        mime_type: "application/vnd.ms-powerpoint",
+      },
+      {
+        type: "file",
+        id: "313233",
+        mime_type: "application/msword",
+      },
+    ];
+
+    const expected: Partial<File>[] = [
+      {
+        type: "file",
+        id: "456",
+        mime_type: "image/gif",
+      },
+      {
+        type: "file",
+        id: "789",
+        mime_type: "image/jpeg",
+      },
+      {
+        type: "file",
+        id: "101112",
+        mime_type: "image/png",
+      },
+      {
+        type: "file",
+        id: "131415",
+        mime_type: "image/svg+xml",
+      },
+      {
+        type: "file",
+        id: "161718",
+        mime_type: "image/webp",
+      },
+    ];
+    expect(processImageFiles(files as File[], "123")).toEqual(expected);
+  });
+
+  it("processImageFiles should support an undefined main image id", () => {
+    const files: Partial<File>[] = [
+      {
+        type: "file",
+        id: "123",
+        mime_type: "image/jpeg",
+      },
+      {
+        type: "file",
+        id: "456",
+        mime_type: "image/gif",
+      },
+    ];
+
+    const expected: Partial<File>[] = [
+      {
+        type: "file",
+        id: "123",
+        mime_type: "image/jpeg",
+      },
+      {
+        type: "file",
+        id: "456",
+        mime_type: "image/gif",
+      },
+    ];
+    expect(processImageFiles(files as File[])).toEqual(expected);
+  });
+
+  it("getProductOtherImageUrls should return a products images files not including the main image", () => {
+    const files: Partial<File>[] = [
+      {
+        type: "file",
+        id: "123",
+        mime_type: "image/jpeg",
+      },
+      {
+        type: "file",
+        id: "456",
+        mime_type: "image/jpeg",
+      },
+    ];
+
+    const mainImageFile: Partial<File> = {
+      type: "file",
+      id: "123",
+      mime_type: "image/jpeg",
+    };
+
+    const productResp: Partial<Resource<ProductResponse>> = {
+      included: {
+        files: files as File[],
+        main_images: [mainImageFile] as File[],
+      },
+    };
+
+    const expected: Partial<File>[] = [
+      {
+        type: "file",
+        id: "456",
+        mime_type: "image/jpeg",
+      },
+    ];
+    expect(
+      getProductOtherImageUrls(productResp as Resource<ProductResponse>)
+    ).toEqual(expected);
+  });
+});
---
+++ lib/product-util.ts
@@ -0,0 +1,26 @@
+import type { File, ProductResponse, Resource } from "@moltin/sdk";
+
+export function processImageFiles(files: File[], mainImageId?: string) {
+  // filters out main image and keeps server order
+  const supportedMimeTypes = [
+    "image/gif",
+    "image/jpeg",
+    "image/png",
+    "image/webp",
+    "image/svg+xml",
+  ];
+  return files.filter(
+    (fileEntry) =>
+      fileEntry.id !== mainImageId &&
+      supportedMimeTypes.some((type) => fileEntry.mime_type === type)
+  );
+}
+
+export function getProductOtherImageUrls(
+  productResp: Resource<ProductResponse>
+): File[] {
+  const files = productResp?.included?.files;
+  return files
+    ? processImageFiles(files, productResp?.included?.main_images?.[0].id)
+    : [];
+}
