d8448338125656bbaaa3fc69768cdb0d06c37d12
butikden <butikxden@gmail.com>
2022-05-04 21:09:16 +0300

    fix: home page updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,96 @@
 import type { NextPage } from "next";
-import styles from "../styles/Home.module.css";
+import styles from "../components/product/carousel/ProductCarousel.module.css";
+import { CarouselProvider, Slide, Slider, Image } from "pure-react-carousel";
+import {
+  StyledButtonBack,
+  StyledButtonNext,
+} from "../components/shared/carousel-wrapped";
+import "pure-react-carousel/dist/react-carousel.es.css";
+import BarStoolImage from "../images/bar-stool.jpeg";
+import BasketImage from "../images/basket.jpeg";
+import BeltImage from "../images/belt.jpeg";
+import LashesKitImage from "../images/lashes-kit.jpeg";
+import PillowImage from "../images/pillow.jpeg";
+import SanitizerImage from "../images/sanitizer.jpeg";
+import SofaImage from "../images/sofa.jpeg";
+import { Grid } from "@chakra-ui/react";
+import LeftArrowIcon from "../components/product/carousel/icons/LeftArrowIcon";
+import RightArrowIcon from "../components/product/carousel/icons/RightArrowIcon";
+import { useState } from "react";
 
 const Home: NextPage<{}> = () => {
+  const images = [
+    BarStoolImage,
+    BasketImage,
+    BeltImage,
+    LashesKitImage,
+    PillowImage,
+    SanitizerImage,
+    SofaImage,
+  ];
+  const selectedStyle = styles["product-carousel-selected"];
+  const baseStyle = styles["horizontal-product-carousel-inner"];
+  const visibleSlides = 5;
+  const shouldDisplayControls = images.length > visibleSlides;
+  const controlsDisplaySettings = shouldDisplayControls ? "flex" : "none";
+  const [selectedProductImage, setSelectedProductImage] = useState(images[0]);
+
   return (
     <div>
       <main className={styles.main}>
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
+        <CarouselProvider
+          visibleSlides={visibleSlides}
+          totalSlides={images.length}
+          step={1}
+          orientation={"horizontal"}
+          naturalSlideWidth={45}
+          naturalSlideHeight={40}
+          infinite={images.length >= visibleSlides}
+          dragEnabled={false}
+        >
+          <Grid
+            gridTemplateColumns={
+              shouldDisplayControls ? "auto 1fr auto" : "1fr"
+            }
+            gap={4}
+            alignItems={"center"}
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+            <StyledButtonBack
+              display={controlsDisplaySettings}
+              justifyContent={"center"}
+            >
+              <LeftArrowIcon />
+            </StyledButtonBack>
+            <Slider>
+              {images.map((image, index) => (
+                <Slide
+                  key={image.src}
+                  index={index}
+                  innerClassName={
+                    image.src === selectedProductImage.src
+                      ? `${selectedStyle} ${baseStyle}`
+                      : baseStyle
+                  }
+                >
+                  <Image
+                    style={{ objectFit: "cover", borderRadius: "0.375rem" }}
+                    hasMasterSpinner={false}
+                    onClick={() => setSelectedProductImage(image)}
+                    alt={image.src}
+                    src={image.src}
+                  />
+                </Slide>
+              ))}
+            </Slider>
+            <StyledButtonNext
+              display={controlsDisplaySettings}
+              justifyContent="center"
+              w="full"
+            >
+              <RightArrowIcon />
+            </StyledButtonNext>
+          </Grid>
+        </CarouselProvider>
       </main>
     </div>
   );
