11150e84fc61286d99b45753723794ef70fbc9df
Robert Field <robertfield@quadrimular.com>
2022-05-06 10:21:49 +0100

    refactor: static product data extracted
    Extracted static product data to external file seperating data
    definition from rendering.
    
    Retained purity of Home component by passing in the products props via
    getStaticProps
    
    Moved images to public as that's where static assets belong.
    
    Issue with static imports outside of the pages directory so compromised
    and used the path string.
    Not ideal but worth the trade off I think.
    See imageSrc inside product-data.ts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,68 +1,24 @@
 import React from "react";
-import type { NextPage } from "next";
+import type { GetStaticProps, NextPage } from "next";
 import { CarouselProvider, Slide, Slider, Image } from "pure-react-carousel";
 import {
   StyledButtonBack,
   StyledButtonNext,
 } from "../components/shared/carousel-wrapped";
 import "pure-react-carousel/dist/react-carousel.es.css";
-import BarStoolImage from "../images/bar-stool.jpeg";
-import BasketImage from "../images/basket.jpeg";
-import BeltImage from "../images/belt.jpeg";
-import LashesKitImage from "../images/lashes-kit.jpeg";
-import PillowImage from "../images/pillow.jpeg";
-import SanitizerImage from "../images/sanitizer.jpeg";
-import SofaImage from "../images/sofa.jpeg";
+
 import { Button, Grid, Text, Box } from "@chakra-ui/react";
 import LeftArrowIcon from "../components/product/carousel/icons/LeftArrowIcon";
 import RightArrowIcon from "../components/product/carousel/icons/RightArrowIcon";
 import styles from "../styles/Home.module.css";
+import { StaticProduct, staticProducts } from "../lib/product-data";
 
-const Home: NextPage<{}> = () => {
+export interface IHome {
+  products: StaticProduct[];
+}
+
+const Home: NextPage<IHome> = ({ products }) => {
   const visibleSlides = 1;
-  const products = [
-    {
-      image: BarStoolImage,
-      title: "Riviera Bar & Counter Stools",
-      subTitle:
-        "The classic Parisian bistro chair, reinvented. True to its roots, each rattan frame is crafted in a time-honored technique perfected by the French, bent and shaped by hand until the iconic shape is achieved.",
-    },
-    {
-      image: SofaImage,
-      title: "Edgewater Sofa",
-      subTitle:
-        "This luxurious performance fabric has a thick weave for great texture and a casual look. Woven from 100% solution dyed acrylic, it will resist all manner of wear and tear - staining, mold, and UV radiation.",
-    },
-    {
-      image: BasketImage,
-      title: "Solid La Jolla Basket",
-      subTitle:
-        "These lidded baskets give storage a beachy spin, with strips of recycled resin wrapped around coils of natural seagrass. Inspired by traditional market baskets, the lightweight design is durable and features...",
-    },
-    {
-      image: PillowImage,
-      title: "Granada Pillow Cover",
-      subTitle:
-        "Inspired by a botanical print from our travels, we pumped up the scale and played with vibrant contrasts to come up with this statement pattern. We love it as the single bold accent in the room, or made more lush by a sea of solids in a complementary hue. With hand-embroidered metallic threads, an exposed brass zip, and a tassel pull, this is clearly all about the details (as any indispensable pillow should be).",
-    },
-    {
-      image: BeltImage,
-      title: "Newman Reversible Leather Belt",
-      subTitle:
-        "Polished geometric buckle hardware provides a smart finish to a fine-grain leather belt that reverses from black to brown.",
-    },
-    {
-      image: LashesKitImage,
-      title: "Lashes Kit",
-      subTitle: "A trio of best-selling products to perfect your lashes.",
-    },
-    {
-      image: SanitizerImage,
-      title: "Travel Size Hand Sanitizer Trio",
-      subTitle:
-        "An exclusive set featuring three travel-size organic hand sanitizers blended with 70% alcohol derived from organic sugarcane.",
-    },
-  ];
 
   return (
     <main className={styles.main}>
@@ -87,9 +43,9 @@ const Home: NextPage<{}> = () => {
             <LeftArrowIcon />
           </StyledButtonBack>
           <Slider>
-            {products.map(({ image, title, subTitle }, index) => (
+            {products.map(({ imageSrc, title, subTitle }, index) => (
               <Slide
-                key={image.src}
+                key={imageSrc}
                 index={index}
                 innerClassName={styles["horizontal-product-carousel-inner"]}
               >
@@ -121,8 +77,8 @@ const Home: NextPage<{}> = () => {
                     flex: "2 1 0",
                   }}
                   hasMasterSpinner={false}
-                  alt={image.src}
-                  src={image.src}
+                  alt={imageSrc}
+                  src={imageSrc}
                 />
               </Slide>
             ))}
@@ -134,6 +90,14 @@ const Home: NextPage<{}> = () => {
       </CarouselProvider>
     </main>
   );
+};
+
+export const getStaticProps: GetStaticProps<IHome> = async () => {
+  return {
+    props: {
+      products: staticProducts,
+    },
+  };
 };
 
 export default Home;
