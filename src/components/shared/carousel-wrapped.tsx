921ea53731fa93c9803b2c97994faa36facf0b34
Robert Field <robertfield@quadrimular.com>
2022-05-06 14:13:38 +0100

    feat: additional wrapping of pure react carousel components

--- src/components/shared/carousel-wrapped.tsx
+++ src/components/shared/carousel-wrapped.tsx
@@ -2,9 +2,11 @@ import { chakra } from "@chakra-ui/react";
 import {
   ButtonBack,
   ButtonNext,
+  Dot,
   Image,
   ImageWithZoom,
   Slide,
+  DotGroup,
 } from "pure-react-carousel";
 
 export const StyledButtonBack = chakra(ButtonBack);
@@ -12,3 +14,5 @@ export const StyledButtonNext = chakra(ButtonNext);
 export const StyledImage = chakra(Image);
 export const StyledImageWithZoom = chakra(ImageWithZoom);
 export const StyledSlide = chakra(Slide);
+export const StyledDot = chakra(Dot);
+export const StyledDotGroup = chakra(DotGroup);
