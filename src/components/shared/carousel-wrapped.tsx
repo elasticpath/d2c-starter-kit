44a32156ea9414362b46408aa33ee8473511c7e3
Robert Field <robertfield@quadrimular.com>
2022-05-06 11:38:04 +0100

    refactor: moved styles onto slide where possible.

--- src/components/shared/carousel-wrapped.tsx
+++ src/components/shared/carousel-wrapped.tsx
@@ -4,9 +4,11 @@ import {
   ButtonNext,
   Image,
   ImageWithZoom,
+  Slide,
 } from "pure-react-carousel";
 
 export const StyledButtonBack = chakra(ButtonBack);
 export const StyledButtonNext = chakra(ButtonNext);
 export const StyledImage = chakra(Image);
 export const StyledImageWithZoom = chakra(ImageWithZoom);
+export const StyledSlide = chakra(Slide);
