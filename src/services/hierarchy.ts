e391f062677da36d50c34904471842180ffdac12
butikden <butikxden@gmail.com>
2022-07-06 13:37:37 +0300

    fix: ShopperCatalog class used instead Caralog

--- src/services/hierarchy.ts
+++ src/services/hierarchy.ts
@@ -8,15 +8,17 @@ import type {
 import { EPCCAPI } from "./helper";
 
 export async function getHierarchies(): Promise<Hierarchy[]> {
-  const result = await EPCCAPI.Catalog.Hierarchies.All();
+  const result = await EPCCAPI.ShopperCatalog.Hierarchies.All();
   const hierarchy = result.data;
   return hierarchy;
 }
 
 export async function getNodes(hierarchyId: string): Promise<Node[]> {
-  const result = (await EPCCAPI.Catalog.Hierarchies.GetHierarchyChildren({
-    hierarchyId,
-  })) as unknown as ResourceList<Node>; // TODO update the js-sdk to use Node instead of Hierarchy
+  const result = (await EPCCAPI.ShopperCatalog.Hierarchies.GetHierarchyChildren(
+    {
+      hierarchyId,
+    }
+  )) as unknown as ResourceList<Node>; // TODO update the js-sdk to use Node instead of Hierarchy
   const nodes = result.data;
   return nodes;
 }
@@ -24,5 +26,5 @@ export async function getNodes(hierarchyId: string): Promise<Node[]> {
 export async function getNodesProducts(
   nodeId: string
 ): Promise<ResourceList<ProductResponse>> {
-  return await EPCCAPI.Catalog.Nodes.GetNodeProducts({ nodeId });
+  return await EPCCAPI.ShopperCatalog.Nodes.GetNodeProducts({ nodeId });
 }
