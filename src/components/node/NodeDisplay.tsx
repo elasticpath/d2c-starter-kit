87ffbb3164b702f73344f0475e974122e51a57d0
Georgios Botzakis <45125702+GeorgeBotzakis@users.noreply.github.com>
2022-07-21 17:51:43 +0100

    feat:  node display component (#26)
    * feat: added NodeDisplay component to index page
    
    * fix: responsiveness fixes and hierarchies addition
    
    * fix: return null if parentNode not found in static props
    
    * chore: fixed formatting
    
    * feat: component implemented and displayed on home screen (#25)
    
    * feat: component implemented and displayed on home screen
    
    * feat: images logic added
    
    * feat: images handling updated
    
    * feat: Update catalog headers and env vars (#24)
    
    * fix: Update get catalog headers. Update SDK
    
    * fix: Update Config
    
    * fix: Update config variables
    
    * fix: Add .env
    
    * fix: Add next public prefix
    
    * fix: Remove console
    
    * refactor: algolia env values check
    
    * refactor: removed REACT_APP prefix
    
    Not needed with nextjs
    
    * refactor: formatting
    
    * feat: runtime env variable check
    
    Check if algolia env vars have been provided
    
    throw descriptive error if they have not
    
    * feat: runtime env variable check stripe
    
    Check if stripe env vars have been provided
    
    throw descriptive error if they have not
    
    * feat: add ep channels and tags
    
    * fix: corrected name of env variable
    
    * fix: corrected name of env variable
    
    * feat: api endpoint for client side promotion fetch
    
    * refactor: camelcase
    
    * refactor: make it clear that this can only be used server side
    
    * fix: workaround for handling serverless epcc js-sdk auth
    
    Warning! This is not a permanent solution. Every time we hit the server side we will be running auth against epcc.
    
    * refactor: clean up async chaining
    
    * fix: hidden secrets
    
    Secrets are now spicified in netlify
    
    to work locally copy the .env.example and name it .env.local
    
    * refactor: marked required values
    
    * chore: updated readme to explain local env
    
    Co-authored-by: Robert Field <robertfield@quadrimular.com>
    
    * fix: products cards wrap (#27)
    
    * fix: removed logs and char from index jsx
    
    * feat: added NodeDisplay component to index page
    
    * fix: responsiveness fixes and hierarchies addition
    
    * fix: return null if parentNode not found in static props
    
    * fix: removed logs and char from index jsx
    
    Co-authored-by: George Botzakis <georgios.botzakis@elasticpath.com>
    Co-authored-by: Denys Butynskyy <33585095+butikden@users.noreply.github.com>
    Co-authored-by: Dubno Petro <34708483+petro97@users.noreply.github.com>
    Co-authored-by: Robert Field <robertfield@quadrimular.com>

---
+++ src/components/node/NodeDisplay.tsx
@@ -0,0 +1,249 @@
+import React, { useEffect, useState, useCallback, useMemo } from "react";
+import {
+  Flex,
+  Text,
+  Box,
+  Link,
+  Heading,
+  Stack,
+  Grid,
+  GridItem,
+  GridItemProps,
+  Skeleton,
+  SkeletonProps,
+} from "@chakra-ui/react";
+import { Node } from "@moltin/sdk";
+import { getNodes, getNodeChildren } from "../../services/hierarchy";
+import { ArrowForwardIcon } from "@chakra-ui/icons";
+import { useRouter } from "next/router";
+
+interface NodeSpecNode {
+  type: "node";
+  data: Node | Node[] | string;
+}
+
+interface NodeSpecHierarchy {
+  type: "hierarchy";
+  data: Node | Node[] | string;
+}
+
+export type NodeSpec = NodeSpecHierarchy | NodeSpecNode; // could be Hierarchy | Node
+
+interface INodeDsiplay {
+  nodeSpec: NodeSpec;
+  title: string;
+  buttonProps?: {
+    link: string;
+    text: string;
+  };
+}
+
+const gridItemStyles: GridItemProps[] = [
+  {
+    rowSpan: 2,
+    /**
+     * padding-bottom trick for aspect ratio boxes
+     * see - https://css-tricks.com/aspect-ratio-boxes/
+     * calc(aspectH / aspectW * 100%)
+     */
+    pb: { base: "50%", md: "100%" },
+  },
+  {
+    pb: { base: "50%", md: "0" },
+    rowSpan: "auto",
+  },
+  {
+    pb: { base: "50%", md: "0" },
+    rowSpan: "auto",
+  },
+];
+
+const skeletonItemStyles: SkeletonProps[] = [
+  {
+    startColor: "blue.500",
+    endColor: "blue.400",
+    rounded: "25px",
+    height: { base: "175px", md: "435px" },
+  },
+  {
+    startColor: "blue.500",
+    endColor: "blue.400",
+    rounded: "25px",
+    height: { base: "175px", md: "200px" },
+  },
+  {
+    startColor: "blue.500",
+    endColor: "blue.400",
+    rounded: "25px",
+    height: { base: "175px", md: "200px" },
+  },
+];
+
+export default function NodeDisplay({
+  nodeSpec,
+  title,
+  buttonProps,
+}: INodeDsiplay) {
+  const router = useRouter();
+  const [nodes, setNodes] = useState<Node[]>([]);
+
+  /**
+   * Either a hierarchy or a node ID
+   */
+  const idSpec = useMemo<string | undefined>(
+    () => (typeof nodeSpec.data === "string" ? nodeSpec.data : undefined),
+    [nodeSpec]
+  );
+
+  const getChildren = useCallback(() => {
+    return nodeSpec.type === "node" ? getNodeChildren : getNodes;
+  }, [nodeSpec]);
+
+  const fetchTopNodes = useCallback(async () => {
+    const nodesArrayResponse = Array.isArray(nodeSpec.data)
+      ? [...nodeSpec.data]
+      : typeof nodeSpec.data === "object"
+      ? [nodeSpec.data]
+      : idSpec != undefined
+      ? await getChildren()(idSpec)
+      : [];
+
+    const topNodes: Node[] = nodesArrayResponse.slice(
+      0,
+      Math.min(3, nodesArrayResponse.length)
+    );
+
+    if (topNodes == null || topNodes.length === 0) {
+      console.log("Not enough nodes to display - setting to empty");
+    }
+
+    setNodes([...topNodes]);
+  }, [nodeSpec, idSpec, getChildren]);
+
+  useEffect(() => {
+    try {
+      fetchTopNodes();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [fetchTopNodes]);
+
+  return (
+    <Stack
+      bg={"#FFAFB"}
+      display={"flex"}
+      maxW={"80rem"}
+      mx="auto"
+      padding={{ base: "2rem", md: "4rem" }}
+    >
+      <Flex
+        justifyContent={"space-between"}
+        alignItems={"baseline"}
+        direction={{ base: "column", sm: "row" }}
+      >
+        <Heading
+          as={"h1"}
+          fontSize={{ base: "1.1rem", md: "1.3rem", lg: "1.5rem" }}
+          fontWeight={"extrabold"}
+        >
+          {title}
+        </Heading>
+        {buttonProps && (
+          <Link
+            color={"#0033CC"}
+            fontWeight={"bold"}
+            fontSize={{ base: "sm", md: "md", lg: "lg" }}
+            onClick={() => {
+              buttonProps.link && router.push(buttonProps.link);
+            }}
+          >
+            {buttonProps.text} <ArrowForwardIcon color={"inherit"} />
+          </Link>
+        )}
+      </Flex>
+      <Grid
+        gap={{ lg: 8 }}
+        columnGap={{ md: 6 }}
+        rowGap={{ base: "1.5rem", md: "6" }}
+        templateRows={{
+          base: "repeat(1,minmax(0,1fr)",
+          md: `repeat(${nodes.length === 1 ? "1" : "2"}, minmax(0,1fr))`,
+        }}
+        templateColumns={{
+          base: "repeat(1,minmax(0,1fr)",
+          md: "repeat(2, minmax(0,1fr))",
+        }}
+        pb={"1em"}
+        px={"1em"}
+      >
+        {nodes.length > 0 ? (
+          nodes.map((node, i) => {
+            return (
+              <GridItem
+                position={"relative"}
+                key={`node-${node.id}`}
+                rounded={"lg"}
+                overflow={"hidden"}
+                {...gridItemStyles[i]}
+                {...(nodes.length === 1 && {
+                  gridColumn: "1 / -1",
+                  rowSpan: 1,
+                  pb: { base: "65%", md: "50%" },
+                })}
+                {...(nodes.length === 2 && {
+                  ...gridItemStyles[0],
+                })}
+              >
+                <Box
+                  position={"absolute"}
+                  w={"100%"}
+                  h={"100%"}
+                  top="0"
+                  left="0"
+                  right="0"
+                  bottom="0"
+                  bgGradient="linear(to-r, gray.300, blue.500)"
+                  _hover={{ opacity: "75%", cursor: "pointer" }}
+                  // placeholder url for categories page
+                  onClick={() => router.push(`/categories/${node.id}`)}
+                />
+                <Flex
+                  fontWeight={"bold"}
+                  bottom={"1rem"}
+                  left={"2rem"}
+                  zIndex={"100"}
+                  position="absolute"
+                  flexDirection={"column"}
+                  justifyContent={"space-between"}
+                >
+                  <Heading fontSize={{ base: "md", md: "1.3rem", lg: "lg" }}>
+                    {node.attributes.name}
+                  </Heading>
+                  <Text
+                    fontWeight={"initial"}
+                    fontSize={{ base: "sm", md: "0.9rem", lg: "1.1rem" }}
+                  >
+                    {node.attributes.description}
+                  </Text>
+                </Flex>
+              </GridItem>
+            );
+          })
+        ) : (
+          <>
+            {skeletonItemStyles &&
+              skeletonItemStyles.map((skeletonProps, index) => {
+                return index === 0 ? (
+                  <GridItem rowSpan={2}>
+                    <Skeleton {...skeletonProps} />
+                  </GridItem>
+                ) : (
+                  <Skeleton {...skeletonProps} />
+                );
+              })}
+          </>
+        )}
+      </Grid>
+    </Stack>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import type { GetStaticProps, NextPage } from "next";
+import type { File, Hierarchy, Node } from "@moltin/sdk";
 import "pure-react-carousel/dist/react-carousel.es.css";
-import type { File } from "@moltin/sdk";
 import { chakra } from "@chakra-ui/react";
+import { getHierarchies, getNodes } from "../services/hierarchy";
 import { StaticProduct, staticProducts } from "../lib/product-data";
 import ProductShowcaseCarousel from "../components/product/carousel/ProductShowcaseCarousel";
 import PromotionBanner, {
@@ -11,9 +12,13 @@ import { getPromotionById } from "../services/promotions";
 import FeaturedProducts from "../components/FeaturedProducts/FeaturedProducts";
 import { getProductsByNode } from "../services/hierarchy";
 import { ProductResponse } from "@moltin/sdk";
+import NodeDisplay from "../components/node/NodeDisplay";
 
 export interface IHome {
   staticProducts: StaticProduct[];
+  hierarchies?: Hierarchy[];
+  parentNode?: Node | null;
+  nodes?: Node[];
   promotion?: PromotionBannerSpec;
   nodeProducts?: ProductResponse[];
   nodeProductsImages?: File[];
@@ -24,6 +29,7 @@ const Home: NextPage<IHome> = ({
   promotion,
   nodeProducts,
   nodeProductsImages,
+  parentNode,
 }) => {
   const nodeId = "4cb5301a-9da3-41a4-9402-c104ed1c2569";
   return (
@@ -41,6 +47,14 @@ const Home: NextPage<IHome> = ({
         nodeProductsImages={nodeProductsImages}
       />
       <ProductShowcaseCarousel products={staticProducts} />
+      <NodeDisplay
+        nodeSpec={{
+          type: "node",
+          data: parentNode?.id ?? "b85de12e-1fe4-4446-96f9-40a7ad641497", // fallback ID belongs to a hierarchy
+        }}
+        buttonProps={{ text: "Browse all categories", link: "/categories" }}
+        title="Shop by Category"
+      ></NodeDisplay>
     </chakra.main>
   );
 };
@@ -51,12 +65,22 @@ export const getStaticProps: GetStaticProps<IHome> = async () => {
   );
   const { data: nodeProducts, included: nodeProductsIncluded } =
     await getProductsByNode("4cb5301a-9da3-41a4-9402-c104ed1c2569");
+
+  const hierarchies = await getHierarchies();
+  const hierarchyChildren =
+    hierarchies.length > 0 ? await getNodes(hierarchies[0].id) : [];
+  // As an example, use first hierarchy's child, if there is one
+  const parentNode =
+    hierarchyChildren.length > 0 ? hierarchyChildren[0] : undefined;
+
   return {
     props: {
       staticProducts,
       promotion,
       nodeProducts: nodeProducts.slice(0, 4),
       nodeProductsImages: nodeProductsIncluded?.main_images,
+      hierarchies,
+      parentNode: parentNode ?? null,
     },
   };
 };
