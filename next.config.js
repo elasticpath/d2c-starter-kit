437854c0c4fdc5d0a9c117c177aeaba3b7f9425d
Robert Field <robertfield@quadrimular.com>
2022-07-25 18:09:45 +0100

    chore: updated image whitelist

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@
  **/
 const nextConfig = {
   images: {
-    domains: ["s3-eu-west-1.amazonaws.com"],
+    domains: ["files-eu.epusercontent.com"],
   },
   webpack(config) {
     config.resolve.fallback = {
