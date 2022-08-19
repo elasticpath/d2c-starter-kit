977ef9f83abe566c405367b617b63fce8206492c
Robert Field <robertfield@quadrimular.com>
2022-08-19 15:16:59 +0100

    feat: nextjs algolia ssr routing workaround
    The NextRouterHandler is a workaround for a current limitation in algolia instantsearch.js
               *  https://github.com/algolia/react-instantsearch/issues/3506#issuecomment-1213341651

---
+++ src/lib/use-next-router-handler.ts
@@ -0,0 +1,139 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+import { useRouter } from "next/router";
+import { useInstantSearch } from "react-instantsearch-hooks-web";
+import { dequal } from "dequal";
+
+import type { UiState } from "instantsearch.js";
+
+export type NextRouterHandlerProps<TRouteParams> = {
+  dynamicRouteQuery?: Record<string, string | string[]>;
+  routeToState: (params: TRouteParams) => UiState;
+  stateToRoute: (uiState: UiState) => TRouteParams;
+  writeDelay?: number;
+};
+
+function removeSearchParams(url: string) {
+  return url.replace(/(\?.+)/, "");
+}
+
+function removeUndefinedParams<TRouteParams>(params: TRouteParams) {
+  return Object.entries(params).reduce((queries, [key, value]) => {
+    if (typeof value !== "undefined") {
+      return { ...queries, [key]: value };
+    }
+    return queries;
+  }, {});
+}
+
+function urlToParams(url: string): Record<string, string | string[]> {
+  return Array.from(new URL(url).searchParams.entries()).reduce<{
+    [key: string]: string | string[];
+  }>((acc, [key, value]) => {
+    const valueAtKey: string | string[] | undefined = acc[key];
+    return {
+      ...acc,
+      [key]: valueAtKey
+        ? Array.isArray(valueAtKey)
+          ? [...valueAtKey, value]
+          : [valueAtKey, value]
+        : value,
+    };
+  }, {});
+}
+
+function NextRouterHandler<
+  TRouteParams extends Record<string, unknown> = Record<string, string>
+>({
+  dynamicRouteQuery = {},
+  routeToState,
+  stateToRoute,
+  writeDelay,
+}: NextRouterHandlerProps<TRouteParams>) {
+  const router = useRouter();
+  const { use, setUiState } = useInstantSearch();
+  const [isLeaving, setIsLeaving] = useState(false);
+  const [stableQuery, setStableQuery] = useState(router?.query || {});
+  const routerPushTimerRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+
+  if (!dequal(stableQuery, router?.query || {})) {
+    setStableQuery(router?.query || {});
+  }
+
+  // Track router path
+  useEffect(() => {
+    function onRouteChangeStart(url: string) {
+      if (removeSearchParams(url) !== removeSearchParams(router.asPath)) {
+        setIsLeaving(true);
+      }
+    }
+
+    router.events.on("routeChangeStart", onRouteChangeStart);
+    return () => router.events.off("routeChangeStart", onRouteChangeStart);
+  }, [router]);
+
+  // Route to state
+  useEffect(() => {
+    const uiState = routeToState(stableQuery as TRouteParams);
+    setUiState(uiState);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [stableQuery]);
+
+  // State to route
+  useEffect(() => {
+    if (isLeaving) {
+      return;
+    }
+
+    return use(() => ({
+      onStateChange({ uiState }) {
+        if (routerPushTimerRef.current) {
+          clearTimeout(routerPushTimerRef.current);
+        }
+        routerPushTimerRef.current = setTimeout(() => {
+          router.push({
+            query: {
+              ...dynamicRouteQuery,
+              ...removeUndefinedParams<TRouteParams>(stateToRoute(uiState)),
+            },
+          });
+        }, writeDelay);
+      },
+    }));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [use, isLeaving]);
+
+  return null;
+}
+
+export function useNextRouterHandler<
+  TRouteParams extends Record<string, unknown> = Record<string, string>
+>({
+  dynamicRouteQuery = {},
+  routeToState,
+  stateToRoute,
+  url,
+  writeDelay = 400,
+}: NextRouterHandlerProps<TRouteParams> & { url: string }) {
+  const [stableDynamicRouteQuery, setStableDynamicRouteQuery] =
+    useState(dynamicRouteQuery);
+
+  if (!dequal(stableDynamicRouteQuery, dynamicRouteQuery)) {
+    setStableDynamicRouteQuery(dynamicRouteQuery);
+  }
+  const initialParams = urlToParams(url) as TRouteParams;
+  const routeParams = initialParams["node"];
+  return {
+    initialUiState: routeToState(urlToParams(url) as TRouteParams),
+    NextRouterHandler: useCallback(
+      () =>
+        NextRouterHandler({
+          dynamicRouteQuery,
+          routeToState,
+          stateToRoute,
+          writeDelay,
+        }),
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      [stableDynamicRouteQuery]
+    ),
+  };
+}
