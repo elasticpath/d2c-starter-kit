624afbcf8bf04c193c14bf1c8c9f19baa73c9ba8
Robert Field <robertfield@quadrimular.com>
2022-08-22 17:00:14 +0100

    refactor: breadcrumb type cleanup

--- src/components/breadcrumb.tsx
+++ src/components/breadcrumb.tsx
@@ -1,14 +1,14 @@
 import { Box, Link, ListItem, OrderedList } from "@chakra-ui/react";
-import { Breadcrumb as BreadcrumbType } from "../lib/create-breadcrumb";
+import { BreadcrumbEntry } from "../lib/create-breadcrumb";
 
 import NextLink from "next/link";
 import { menuItemStyleProps } from "../lib/menu-style";
 
 interface IBreadcrumb {
-  breadcrumbs: BreadcrumbType[];
+  entries: BreadcrumbEntry[];
 }
 
-export default function Breadcrumb({ breadcrumbs }: IBreadcrumb): JSX.Element {
+export default function Breadcrumb({ entries }: IBreadcrumb): JSX.Element {
   return (
     <OrderedList
       display={"flex"}
@@ -17,25 +17,24 @@ export default function Breadcrumb({ breadcrumbs }: IBreadcrumb): JSX.Element {
       listStyleType={"none"}
       m={"0"}
     >
-      {breadcrumbs.length > 1 &&
-        breadcrumbs.map((crumb, index, array) => (
-          <ListItem key={crumb.value}>
-            {array.length === index + 1 ? (
-              <Box as="span" fontWeight={"bold"}>
-                {crumb.value}
-              </Box>
-            ) : (
-              <NextLink href={`/search/${crumb.breadcrumb}`} passHref>
-                <Link {...menuItemStyleProps}>{crumb.value}</Link>
-              </NextLink>
-            )}
-            {array.length !== index + 1 && (
-              <Box as="span" ml={4}>
-                /
-              </Box>
-            )}
-          </ListItem>
-        ))}
+      {entries.map((entry, index, array) => (
+        <ListItem key={entry.value}>
+          {array.length === index + 1 ? (
+            <Box as="span" fontWeight={"bold"}>
+              {entry.value}
+            </Box>
+          ) : (
+            <NextLink href={`/search/${entry.breadcrumb}`} passHref>
+              <Link {...menuItemStyleProps}>{entry.value}</Link>
+            </NextLink>
+          )}
+          {array.length !== index + 1 && (
+            <Box as="span" ml={4}>
+              /
+            </Box>
+          )}
+        </ListItem>
+      ))}
     </OrderedList>
   );
 }
