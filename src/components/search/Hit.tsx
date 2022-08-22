f660a398c645657699ad27e3dd79c191db8ec6c4
Robert Field <robertfield@quadrimular.com>
2022-08-22 16:54:11 +0100

    refactor: moved hit to its own component

---
+++ src/components/search/Hit.tsx
@@ -0,0 +1,70 @@
+import { SearchHit } from "./SearchHit";
+import {
+  Center,
+  Grid,
+  GridItem,
+  Heading,
+  Image,
+  LinkBox,
+  LinkOverlay,
+  Text,
+} from "@chakra-ui/react";
+import { ViewOffIcon } from "@chakra-ui/icons";
+import Link from "next/link";
+
+export default function HitComponent({ hit }: { hit: SearchHit }): JSX.Element {
+  const {
+    ep_price,
+    ep_name,
+    ep_slug,
+    objectID,
+    ep_main_image_url,
+    ep_description,
+  } = hit;
+  return (
+    <LinkBox display={"grid"} gridTemplateRows="auto 1fr" h="full">
+      <GridItem position="relative" overflow="hidden" pb="100%">
+        {ep_main_image_url ? (
+          <Image
+            boxSize="100%"
+            position="absolute"
+            objectFit="cover"
+            src={ep_main_image_url}
+            alt={ep_name}
+            roundedTop="lg"
+          />
+        ) : (
+          <Center
+            w="100%"
+            h="100%"
+            bg="gray.200"
+            color="white"
+            position="absolute"
+          >
+            <ViewOffIcon w="10" h="10" />
+          </Center>
+        )}
+      </GridItem>
+      <Grid gridTemplateRows="auto 1fr auto" gap={2} p={4}>
+        <Heading size="sm">
+          <Link href={`/products/${ep_slug}/${objectID}`} passHref>
+            <LinkOverlay>{ep_name}</LinkOverlay>
+          </Link>
+        </Heading>
+        <Text
+          color="gray.500"
+          fontWeight="semibold"
+          letterSpacing="wide"
+          fontSize="xs"
+          mt="1"
+          noOfLines={6}
+        >
+          {ep_description}
+        </Text>
+        <Text fontSize="md" fontWeight="semibold" mt="1">
+          {ep_price}
+        </Text>
+      </Grid>
+    </LinkBox>
+  );
+}
--- src/components/search/Hits.tsx
+++ src/components/search/Hits.tsx
@@ -1,78 +1,11 @@
-import { ViewOffIcon } from "@chakra-ui/icons";
-import {
-  Center,
-  Grid,
-  GridItem,
-  Heading,
-  LinkBox,
-  LinkOverlay,
-  Image,
-  Text,
-} from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 import { useHits } from "react-instantsearch-hooks-web";
 import { SearchHit } from "./SearchHit";
-import Link from "next/link";
 import NoResults from "./NoResults";
+import HitComponent from "./Hit";
 
 export default function Hits(): JSX.Element {
   const { hits } = useHits<SearchHit>();
-
-  const HitComponent = ({ hit }: { hit: SearchHit }) => {
-    const {
-      ep_price,
-      ep_name,
-      ep_slug,
-      objectID,
-      ep_main_image_url,
-      ep_description,
-    } = hit;
-    return (
-      <LinkBox display={"grid"} gridTemplateRows="auto 1fr" h="full">
-        <GridItem position="relative" overflow="hidden" pb="100%">
-          {ep_main_image_url ? (
-            <Image
-              boxSize="100%"
-              position="absolute"
-              objectFit="cover"
-              src={ep_main_image_url}
-              alt={ep_name}
-              roundedTop="lg"
-            />
-          ) : (
-            <Center
-              w="100%"
-              h="100%"
-              bg="gray.200"
-              color="white"
-              position="absolute"
-            >
-              <ViewOffIcon w="10" h="10" />
-            </Center>
-          )}
-        </GridItem>
-        <Grid gridTemplateRows="auto 1fr auto" gap={2} p={4}>
-          <Heading size="sm">
-            <Link href={`/products/${ep_slug}/${objectID}`} passHref>
-              <LinkOverlay>{ep_name}</LinkOverlay>
-            </Link>
-          </Heading>
-          <Text
-            color="gray.500"
-            fontWeight="semibold"
-            letterSpacing="wide"
-            fontSize="xs"
-            mt="1"
-            noOfLines={6}
-          >
-            {ep_description}
-          </Text>
-          <Text fontSize="md" fontWeight="semibold" mt="1">
-            {ep_price}
-          </Text>
-        </Grid>
-      </LinkBox>
-    );
-  };
 
   if (hits.length) {
     return (
