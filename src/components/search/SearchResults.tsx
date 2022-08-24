66dbb5b1195aa0d4370e6e391c0cc0efc8f2dfa8
Robert Field <robertfield@quadrimular.com>
2022-08-24 14:22:06 +0100

    feat: added the mobile filter menu and made style changes
    - mobile filter menu added
    - moved pagination inside of hits section
    - changed default title to "All Categories"
    - breakpoints styling

--- src/components/search/SearchResults.tsx
+++ src/components/search/SearchResults.tsx
@@ -14,17 +14,32 @@ import {
   Text,
   Heading,
 } from "@chakra-ui/react";
-import {
-  SearchBox,
-  useSortBy,
-  useInstantSearch,
-} from "react-instantsearch-hooks-web";
+import { useSortBy, useInstantSearch } from "react-instantsearch-hooks-web";
 import { algoliaEnvData } from "../../lib/resolve-algolia-env";
 import CustomHierarchicalMenu from "./CustomHierarchicalMenu";
 import Hits from "./Hits";
 import Pagination from "./Pagination";
+import { BreadcrumbLookup } from "../../lib/types/breadcrumb-lookup";
+import SearchBox from "./SearchBox";
+import MobileFilters from "./MobileFilters";
+import { hierarchicalAttributes } from "../../lib/hierarchical-attributes";
 
-export default function SearchResults(): JSX.Element {
+interface ISearchResults {
+  lookup?: BreadcrumbLookup;
+  NextRouterHandler: any;
+}
+
+function resolveTitle(slugArray: string[], lookup?: BreadcrumbLookup): string {
+  return (
+    lookup?.[`/${slugArray.join("/")}`]?.name ??
+    slugArray[slugArray?.length - 1]
+  );
+}
+
+export default function SearchResults({
+  lookup,
+  NextRouterHandler,
+}: ISearchResults): JSX.Element {
   const { uiState } = useInstantSearch();
 
   const { options, refine } = useSortBy({
@@ -43,58 +58,71 @@ export default function SearchResults(): JSX.Element {
 
   const { hierarchicalMenu, query } = uiState[algoliaEnvData.indexName];
   const slugArray = hierarchicalMenu?.["ep_slug_categories.lvl0"];
-  const title = slugArray && slugArray[slugArray?.length - 1];
+
+  const title = slugArray ? resolveTitle(slugArray, lookup) : "All Categories";
 
   return (
-    <Grid gap={2} maxW="7xl" mx="auto">
-      <Flex minWidth="max-content" alignItems="center" gap="2" pt={8}>
+    <Grid gap={4} maxW="7xl" mx="auto">
+      <Flex alignItems="center" gap="2" pt={8} wrap={"wrap"}>
         <Box py="2">
-          <Heading>{title ? title : "Search"}</Heading>
+          <Heading>{title}</Heading>
           {query && <Text>Search results for &quot;{query}&quot;</Text>}
         </Box>
         <Spacer />
-        <Box py="2">
-          <Menu>
-            <MenuButton
-              as={Button}
-              variant="ghost"
-              rightIcon={<ChevronDownIcon />}
-            >
-              Sort
-            </MenuButton>
-            <MenuList>
-              {options.map((option) => (
-                <MenuItem
-                  key={option.value}
-                  onClick={() => refine(option.value)}
-                >
-                  {option.label}
-                </MenuItem>
-              ))}
-            </MenuList>
-          </Menu>
-        </Box>
+        <Flex alignItems="center" gap={2}>
+          <MobileFilters
+            lookup={lookup}
+            NextRouterHandler={NextRouterHandler}
+          />
+          <Box py="2">
+            <Menu>
+              <MenuButton
+                as={Button}
+                variant="ghost"
+                rightIcon={<ChevronDownIcon />}
+              >
+                Sort
+              </MenuButton>
+              <MenuList zIndex="dropdown">
+                {options.map((option) => (
+                  <MenuItem
+                    key={option.value}
+                    onClick={() => refine(option.value)}
+                  >
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </MenuList>
+            </Menu>
+          </Box>
+        </Flex>
       </Flex>
+
+      <Box>
+        <SearchBox />
+      </Box>
       <Divider />
-      <SearchBox />
       <Grid templateColumns={{ base: "1fr", md: "auto 1fr" }} gap={8}>
-        <GridItem display={{ base: "none", md: "block" }}>
+        <GridItem
+          minWidth={{ base: "3xs", lg: "2xs" }}
+          display={{ base: "none", md: "block" }}
+        >
+          <Heading as="h3" size="sm" pb={2}>
+            Category
+          </Heading>
           <CustomHierarchicalMenu
-            attributes={[
-              "ep_slug_categories.lvl0",
-              "ep_slug_categories.lvl1",
-              "ep_slug_categories.lvl2",
-              "ep_slug_categories.lvl3",
-            ]}
+            lookup={lookup}
+            attributes={hierarchicalAttributes}
           />
         </GridItem>
 
         <GridItem>
           <Hits />
+          <Box py={10}>
+            <Pagination />
+          </Box>
         </GridItem>
       </Grid>
-
-      <Pagination />
     </Grid>
   );
 }
