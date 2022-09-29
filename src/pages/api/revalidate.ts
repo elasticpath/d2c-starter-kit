0587a47fd32a6a051da818843a31bb454c45c024
Denys Butynskyy <denys.butynskyy@elasticpath.com>
2022-09-29 17:19:02 +0300

    fix: check token temporary removed

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -2,9 +2,9 @@ export default async function handler(req: any, res: any) {
   console.warn(req, "req");
   console.warn(res, "res");
   // Check for secret to confirm this is a valid request
-  if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
+  /*  if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
     return res.status(401).json({ message: "Invalid token" });
-  }
+  }*/
 
   try {
     await res.revalidate("/products/23ff5935-25bc-4729-a770-8ff551e46188");
