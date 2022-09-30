387e23423bacd280d4e2a4f3cf7d4bb0dd18d160
Denys Butynskyy <denys.butynskyy@elasticpath.com>
2022-09-30 13:47:27 +0300

    fix: res changed

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -1,6 +1,6 @@
 export default async function handler(req: any, res: any) {
-  console.warn(req, "req");
-  console.warn(res, "res");
+  //console.warn(req, "req");
+  //console.warn(res, "res");
   // Check for secret to confirm this is a valid request
   /*  if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
     return res.status(401).json({ message: "Invalid token" });
@@ -8,8 +8,8 @@ export default async function handler(req: any, res: any) {
 
   try {
     await res.revalidate("/products/23ff5935-25bc-4729-a770-8ff551e46188");
-    return res.json({ revalidated: true, req });
+    return res.json({ revalidated: true, query: req.query });
   } catch (err) {
-    return res.status(500).send(err, "Error revalidating");
+    return res.status(500).json({ message: err });
   }
 }
