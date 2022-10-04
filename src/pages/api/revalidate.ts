d59e74591710fbe81cb7d2777bd9e63347b9ab82
Denys Butynskyy <denys.butynskyy@elasticpath.com>
2022-10-04 11:27:03 +0300

    feat: revalidate function fully implemented

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -1,12 +1,11 @@
 import { getCatalogReleaseById } from "../../services/catalog";
-import { unzipBlobToJson } from "../../lib/unzipBlobToJson";
+import { unzipBlobToString } from "../../lib/unzipBlobToString";
 
 // @ts-ignore
 export default async function handler(req: any, res: any) {
-  // Check for secret to confirm this is a valid request
-  /*  if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
+  if (req.query.secret !== process.env.NEXT_PUBLIC_REVALIDATE_SECRET_TOKEN) {
     return res.status(401).json({ message: "Invalid token" });
-  }*/
+  }
 
   try {
     const catalogId = req.body.payload.attributes.catalog_id;
@@ -16,7 +15,14 @@ export default async function handler(req: any, res: any) {
     const deltaFileUrl = releaseResp.data.relationships.delta.links.related;
     const resp = await fetch(deltaFileUrl);
     const blob = await resp.blob();
-    const json = await unzipBlobToJson(blob);
+    const unzipped = await unzipBlobToString(blob);
+    const productsArr = unzipped.split("\n");
+    for (const str of productsArr) {
+      if (str) {
+        const product = JSON.parse(str);
+        await res.revalidate(`/products/${product.id}`);
+      }
+    }
     return res.json({ revalidated: true });
   } catch (err) {
     return res.status(500).json({ message: err });
