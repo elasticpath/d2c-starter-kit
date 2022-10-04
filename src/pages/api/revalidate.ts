5de15e7eec4e1372e5cfc853ba49bdf096fd0486
Denys Butynskyy <denys.butynskyy@elasticpath.com>
2022-10-04 16:14:58 +0300

    fix: debug

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -10,16 +10,20 @@ export default async function handler(
     return res.status(401).json({ message: "Invalid token" });
   }
 
-  let deltaFileResp, fileBlob, unzipped, productsArr;
   try {
     const catalogId = req.body.payload.attributes.catalog_id;
     const releaseId = req.body.payload.id;
     const releaseResp = await getCatalogReleaseById(catalogId, releaseId);
     const deltaFileUrl = releaseResp.data.relationships.delta.links.related;
-    deltaFileResp = await fetch(deltaFileUrl);
-    fileBlob = await deltaFileResp.blob();
-    unzipped = await unzipBlobToString(fileBlob);
-    productsArr = unzipped.split("\n");
+    let deltaFileResp;
+    try {
+      deltaFileResp = await fetch(deltaFileUrl);
+    } catch (e) {
+      return res.status(500).json({ e, deltaFileUrl });
+    }
+    const fileBlob = await deltaFileResp.blob();
+    const unzipped = await unzipBlobToString(fileBlob);
+    const productsArr = unzipped.split("\n");
     for (const str of productsArr) {
       if (str) {
         const product = JSON.parse(str);
@@ -30,10 +34,6 @@ export default async function handler(
   } catch (err) {
     return res.status(500).json({
       message: err,
-      deltaFileResp,
-      fileBlob,
-      unzipped,
-      productsArr,
     });
   }
 }
