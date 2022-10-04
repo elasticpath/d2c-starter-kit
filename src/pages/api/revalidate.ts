d244d1ed4d2b0a7fb810a91c99d6df74dc040961
Denys Butynskyy <denys.butynskyy@elasticpath.com>
2022-10-04 14:28:40 +0300

    fix: env variable name changed

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -6,7 +6,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.query.secret !== process.env.NEXT_PUBLIC_REVALIDATE_SECRET_TOKEN) {
+  if (req.query.secret !== process.env.REVALIDATE_SECRET_TOKEN) {
     return res.status(401).json({ message: "Invalid token" });
   }
 
