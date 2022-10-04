43e730030aa9ea721b6139cb227f853ea93551b2
Denys Butynskyy <denys.butynskyy@elasticpath.com>
2022-10-04 13:54:23 +0300

    feat: sdk version up and cleanup

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -1,8 +1,11 @@
 import { getCatalogReleaseById } from "../../services/catalog";
 import { unzipBlobToString } from "../../lib/unzipBlobToString";
+import { NextApiRequest, NextApiResponse } from "next";
 
-// @ts-ignore
-export default async function handler(req: any, res: any) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.query.secret !== process.env.NEXT_PUBLIC_REVALIDATE_SECRET_TOKEN) {
     return res.status(401).json({ message: "Invalid token" });
   }
@@ -11,11 +14,10 @@ export default async function handler(req: any, res: any) {
     const catalogId = req.body.payload.attributes.catalog_id;
     const releaseId = req.body.payload.id;
     const releaseResp = await getCatalogReleaseById(catalogId, releaseId);
-    //@ts-ignore
     const deltaFileUrl = releaseResp.data.relationships.delta.links.related;
-    const resp = await fetch(deltaFileUrl);
-    const blob = await resp.blob();
-    const unzipped = await unzipBlobToString(blob);
+    const deltaFileResp = await fetch(deltaFileUrl);
+    const fileBlob = await deltaFileResp.blob();
+    const unzipped = await unzipBlobToString(fileBlob);
     const productsArr = unzipped.split("\n");
     for (const str of productsArr) {
       if (str) {
--- src/services/catalog.ts
+++ src/services/catalog.ts
@@ -1,17 +1,14 @@
 import type { Resource, ShopperCatalogReleaseBase } from "@moltin/sdk";
 import { epccServerClient } from "../lib/epcc-server-client";
-import { Moltin as EPCCClient } from "@moltin/sdk";
-import { getEpccImplicitClient } from "../lib/epcc-implicit-client";
 
 /**
  * Get a catalog release by id
  */
 export async function getCatalogReleaseById(
   catalogId: string,
-  releaseId: string,
-  client?: EPCCClient
+  releaseId: string
 ): Promise<Resource<ShopperCatalogReleaseBase>> {
-  return (client ?? getEpccImplicitClient()).Catalogs.Releases.Get({
+  return await epccServerClient.Catalogs.Releases.Get({
     catalogId,
     releaseId,
   });
