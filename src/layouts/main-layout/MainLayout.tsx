2997e3218de3ee0b4766ec297d3d76f5830c2b43
Robert Field <robertfield@quadrimular.com>
2022-11-19 08:35:09 +0000

    feat: added catch all page title and meta
    Still need to add a [customizable layout](https://github.com/vercel/next.js/blob/canary/examples/layout-component/pages/_app.tsx) for each page.

--- src/layouts/main-layout/MainLayout.tsx
+++ src/layouts/main-layout/MainLayout.tsx
@@ -4,6 +4,7 @@ import Footer from "../../components/footer/Footer";
 import type { ReactNode } from "react";
 import type { NavigationNode } from "../../lib/build-site-navigation";
 import { Toaster } from "../../components/toast/toaster";
+import Head from "next/head";
 
 interface IMainLayout {
   nav: NavigationNode[];
@@ -13,6 +14,13 @@ interface IMainLayout {
 const MainLayout = ({ nav, children }: IMainLayout): JSX.Element => {
   return (
     <>
+      <Head>
+        <title>D2C Starter Kit</title>
+        <meta
+          name="description"
+          content="D2C Starter Kit - a store front starter for Elastic Path Commerce Cloud"
+        />
+      </Head>
       <Toaster />
       <Header nav={nav} />
       {children}
