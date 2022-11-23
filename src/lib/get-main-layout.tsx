fad11cfe5f798d027bb734cef824a6007deb3fea
Robert Field <robertfield@quadrimular.com>
2022-11-23 13:59:12 +0000

    refactor: moved layouts inside the components folder
    - Layouts belong as components.

--- src/lib/get-main-layout.tsx
+++ src/lib/get-main-layout.tsx
@@ -1,4 +1,4 @@
-import MainLayout from "../layouts/MainLayout";
+import MainLayout from "../components/layouts/MainLayout";
 import { GetLayoutFn } from "../pages/_app";
 
 export const getMainLayout: GetLayoutFn = (page, ctx) => {
