2ec1946b6cf266ffba9e136bfb82eeb435fab4fa
Robert Field <robertfield@quadrimular.com>
2022-11-23 13:51:23 +0000

    feat: added support for easy custom layouts.
    - default layout is still MainLayout component

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,24 @@
+import Header from "../components/header/Header";
+import Footer from "../components/footer/Footer";
+
+import type { ReactNode } from "react";
+import type { NavigationNode } from "../lib/build-site-navigation";
+import { Toaster } from "../components/toast/toaster";
+
+interface IMainLayout {
+  nav?: NavigationNode[];
+  children: ReactNode;
+}
+
+const MainLayout = ({ nav = [], children }: IMainLayout): JSX.Element => {
+  return (
+    <>
+      <Toaster />
+      <Header nav={nav} />
+      {children}
+      <Footer />
+    </>
+  );
+};
+
+export default MainLayout;
