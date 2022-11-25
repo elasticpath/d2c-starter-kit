d2deac310bc52a1baa9ba500cd56ff866c70da60
Denys Butynskyy <denys.butynskyy@elasticpath.com>
2022-11-25 13:26:38 +0200

    fix: correct types for range field applied

--- src/lib/algolia-search-routing.ts
+++ src/lib/algolia-search-routing.ts
@@ -11,11 +11,15 @@ export function resolveRouting(
   return {
     dynamicRouteQuery: {},
     url,
-    // @ts-ignore
     routeToState(routeState) {
       //  stateNode set to default to node for initial direct navigation render
-      const { query, page, node: stateNode = node, sortBy, range } = routeState;
-
+      const {
+        query,
+        page,
+        node: stateNode = node,
+        sortBy,
+        range = "",
+      } = routeState;
       return {
         [algoliaEnvData.indexName]: {
           query: query,
@@ -26,21 +30,21 @@ export function resolveRouting(
               : [stateNode],
           },
           sortBy: sortBy,
-          range: { "ep_price.USD.float_price": range },
+          range: {
+            "ep_price.USD.float_price": range,
+          },
         },
       };
     },
-    //@ts-ignore
     stateToRoute(uiState) {
       const indexUiState = uiState[algoliaEnvData.indexName] || {};
       const { query, page, hierarchicalMenu, sortBy, range } = indexUiState;
-
       return {
         query,
         page,
         sortBy,
         node: hierarchicalMenu?.[EP_ROUTE_CATEGORY],
-        range: range ? range["ep_price.USD.float_price"] : undefined,
+        range: range?.["ep_price.USD.float_price"],
       };
     },
   };
