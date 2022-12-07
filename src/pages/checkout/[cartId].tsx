b1b0c87ba2e8464f04bee40c009704b0f253a055
Denys Butynskyy <denys.butynskyy@elasticpath.com>
2022-12-07 14:35:33 +0200

    feat: manual gateway checkout

--- src/pages/checkout/[cartId].tsx
+++ src/pages/checkout/[cartId].tsx
@@ -21,7 +21,7 @@ import { OrderCompleteState } from "../../components/checkout/types/order-pendin
 import { PresentCartState } from "@elasticpath/react-shopper-hooks";
 import { CheckoutForm as CheckoutFormType } from "../../components/checkout/form-schema/checkout-form-schema";
 import OrderComplete from "../../components/cart/OrderComplete";
-import StripeTypeCheckoutForm from "../../components/checkout/StripeTypeCheckoutForm";
+import ManualTypeCheckoutForm from "../../components/checkout/ManualTypeCheckoutForm";
 
 interface ICheckout {
   cart: ResourceIncluded<Cart, CartIncluded>;
@@ -82,7 +82,7 @@ export const Checkout: NextPage<ICheckout> = () => {
                 colStart={{ base: 1, md: 1 }}
               >
                 {showEpStripePaymentGateway ? (
-                  <StripeTypeCheckoutForm
+                  <ManualTypeCheckoutForm
                     checkout={stripeIntent}
                     showCompletedOrder={showCompletedOrder(presentCart)}
                   />
