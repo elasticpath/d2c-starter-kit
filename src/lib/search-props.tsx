89241b34acd3bda6aeefe0c52ac5d951eef542eb
Denys Butynskyy <denys.butynskyy@elasticpath.com>
2022-12-08 22:28:57 +0200

    fix: NextPage interface instead of NextPageWithLayout on getSearchSSRProps function

--- src/lib/search-props.tsx
+++ src/lib/search-props.tsx
@@ -1,14 +1,13 @@
 import type { GetServerSideProps } from "next";
+import type { NextPage } from "next";
 import type { ParsedUrlQuery } from "querystring";
 import type { InstantSearchServerState } from "react-instantsearch-hooks-web";
 
 import { getServerState } from "react-instantsearch-hooks-server";
-import type { FunctionComponent } from "react";
 import React from "react";
 import { BreadcrumbEntry, createBreadcrumb } from "./create-breadcrumb";
 import { BreadcrumbLookup } from "./types/breadcrumb-lookup";
 import { renderToString } from "react-dom/server";
-import { NextPageWithLayout } from "../pages/_app";
 
 export interface SearchQuery extends ParsedUrlQuery {
   nodeId: string;
@@ -24,7 +23,7 @@ export interface ISearch {
 
 export const getSearchSSRProps =
   (
-    SearchComponent: NextPageWithLayout<ISearch>,
+    SearchComponent: NextPage<ISearch>,
     lookup: BreadcrumbLookup
   ): GetServerSideProps<ISearch, SearchQuery> =>
   async ({ req, params }) => {
