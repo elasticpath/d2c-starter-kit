0420ca652dc83a890d4cd715519ef898d602833a
Neil Taylor <neil.taylor@elasticpath.com>
2023-02-16 11:40:12 +0000

    Tidy up

--- src/pages/about.tsx
+++
@@ -1,8 +0,0 @@
-import { withStoreStaticProps } from "../lib/store-wrapper-ssg";
-import Blurb from "../components/shared/blurb-about";
-
-const About = () => <Blurb title="About" />;
-
-export default About;
-
-export const getServerSideProps = withStoreStaticProps();
